2238ae3820583026e7d19d3c1a32fe248d1ba357
Irina <girishkaizh@mail.ru>
2025-08-26 17:40:29 +0400

    refactor

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -7,10 +7,10 @@ from brain_games.games.brain_game import brain_game
 
 def brain_progression_game():
     game_config = {
-        "rules": 'What number is missing in the progression?',
-        "generate_question": generate_question,
-        "get_correct_answer": get_correct_answer,
-        "get_user_answer": lambda: prompt.integer("Your answer: "),
+        'rules': 'What number is missing in the progression?',
+        'generate_question': generate_question,
+        'get_correct_answer': get_correct_answer,
+        'get_user_answer': lambda: prompt.integer('Your answer: '),
     }
 
     brain_game(**game_config)
@@ -18,7 +18,7 @@ def brain_progression_game():
 
 def generate_question():
     progression = get_progression()
-    question = " ".join(map(lambda x: str(x) if x is not None else "..", progression))
+    question = ' '.join('..' if x is None else str(x) for x in progression)
     return question
 
 
@@ -41,7 +41,7 @@ def get_progression():
 
 
 def get_correct_answer(question):
-    progression = list(map(lambda x: None if x == '..' else int(x), question.split()))
+    progression = [None if x == '..' else int(x) for x in question.split()]
     return find_missing_number(progression)
 
 
@@ -49,7 +49,7 @@ def find_missing_number(progression):
     for i, num in enumerate(progression):
         if num is not None:
             continue
- 
+
         if i == 0:
             _, second, third = progression[:3]
             step = third - second
@@ -60,7 +60,7 @@ def find_missing_number(progression):
             step = second_from_end - third_from_end
             return second_from_end + step
 
-        previous = progression[i - 1]
-        next = progression[i + 1]
-        step = (next - previous) // 2
-        return previous + step
+        prev_num = progression[i - 1]
+        next_num = progression[i + 1]
+        step = (next_num - prev_num) // 2
+        return prev_num + step
